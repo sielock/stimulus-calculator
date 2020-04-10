4416186fd4b15d91888cdf47f0f382bba287cfd0
sielock <aaboarder@gmail.com>
2020-04-10 17:03:03 -0600

    adjusted calculator function to account for singles making < 75000

--- src/stimcalc.py
+++ src/stimcalc.py
@@ -9,7 +9,9 @@ def amount(income, status, kids):
     income = float(income)
     amt = 0
 
-    if status == "Single" and 75000 >= income < 99000 :
+    if status == "Single" and income < 75000:
+        amt = 1200 + kids
+    elif status == "Single" and 75000 <= income < 99000 :
         amt = 1200 - abs((income - 75000)*0.05) + kids
     elif status == "Married" and income < 150000:
         amt = 2400 + kids
